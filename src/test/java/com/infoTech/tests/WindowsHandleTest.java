f9501237cd0ba06917219a5a8bafe57abd422cf9
Ekremteke <ekremteke.uk@gmail.com>
2022-05-24 13:56:23 +0100

    Windows handle tests

--- src/test/java/com/infoTech/tests/WindowsHandleTest.java
+++ src/test/java/com/infoTech/tests/WindowsHandleTest.java
@@ -1,4 +1,40 @@
 package com.infoTech.tests;
 
-public class WindowsHandleTest {
+import com.infoTech.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class WindowsHandleTest extends TestBase {
+    @Test
+    public void WindowsHandle() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/windows");
+
+        String handle = driver.getWindowHandle();
+        System.out.println(handle);
+
+       driver.findElement(By.partialLinkText("Click Here")).click();
+       // element.click();
+
+        //Tum sayfalarin handle'ini aliyoruz
+
+        Set<String> tumElementler = driver.getWindowHandles();
+
+        for (String w:tumElementler) {
+            System.out.println(w);
+        }
+
+        //Set olan listemizi once Array'e cevriyruz
+        Object[] array=(Object[]) tumElementler.toArray();
+
+        //istedigimiz sayfanin index'ini yazdirip cagiriyoruz..
+        String sonSayfaHandle = array[array.length-2].toString();
+        System.out.println("Son sayfa handle => " + sonSayfaHandle);
+
+        Thread.sleep(2000);
+        driver.switchTo().window(handle);
+
+    }
 }
